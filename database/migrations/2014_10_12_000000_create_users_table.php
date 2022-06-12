3807c1dee1c9f9921fff43fa22b3329574784613
Marco Martínez Arenas <marcomartinez@signlab.info>
2022-06-12 15:25:26 +0200

    se ha quitado restricción unique() a campo email de tabla users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('surname');
             $table->string('username');
             $table->boolean('is_admin')->default(0);
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->date('date_of_birth')->nullable();
