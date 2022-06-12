b120afed7a7698fbea93b2fe1fd39ced73c58ce1
Marco Martínez Arenas <marcomartinez@signlab.info>
2022-06-12 13:31:53 +0200

    cambio bootstrap.js pusher

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,8 +22,8 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.PUSHER_APP_KEY,
-    cluster: process.env.PUSHER_APP_CLUSTER,
+    key: f2c7285a6b796f6a80b1,
+    cluster: eu,
     forceTLS: true,
     disableStats: true,
     auth: {
