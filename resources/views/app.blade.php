54159ea1473928e7bfc5d5f63495b4fc0f77c2b6
Marco Martínez Arenas <marcomartinez@signlab.info>
2022-06-12 20:44:39 +0200

    redis

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,15 +21,4 @@
     <body class="font-primary antialiased">
         @inertia
     </body>
-    <script>
-    
-        var pusher = new Pusher('f2c7285a6b796f6a80b1', {
-          cluster: 'eu'
-        });
-    
-        var channel = pusher.subscribe('my-channel');
-        channel.bind('my-event', function(data) {
-          alert(JSON.stringify(data));
-        });
-      </script>
 </html>
