cd2fcb7222b398bda7bc6ca6f5804901943fb94c
Marco Martínez Arenas <marcomartinez@signlab.info>
2022-06-13 01:46:23 +0200

    cambio appLayout para logo.png

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -13,7 +13,7 @@
                                     class="h-4/5 w-14"
                                 >
                                     <img
-                                        :src="'/public/storage/images/logo.png'"
+                                        :src="'/storage/images/logo.png'"
                                         alt="Logo"
                                     />
                                 </Link>
